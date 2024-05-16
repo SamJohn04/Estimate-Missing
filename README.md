5416b0883c6c3ecec093dfa29d57996e4347c15e
SamJohn04 <samuel.john.codes@gmail.com>
2024-05-16 20:31:33 +0530

    refactor: estimator.py to root dir

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ESTIMATE MISSING
+Tool to estimate missing (nan) values. Analyze the category of values that are missing and use appropriate ML Model to predict the values.
+
+## Libraries Used
+- NumPy
+- Pandas
+- Scikit-learn
+
+## Usage
+- CLI: python3 main.py _input-file_ _output-file_
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from src.estimator import Estimator
+from estimator import Estimator
 import sys
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         estimator = Estimator(data_src=sys.argv[1])
     else:
-        estimator = Estimator(data_src='test/data.csv')
+        print('No Input File given')
+        exit()
     new_data = estimator.fill_missing_values()
     if len(sys.argv) > 2:
         new_data.to_csv(sys.argv[2], index=False)
